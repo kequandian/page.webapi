39968f906d3c301877a706e412d73089139340d7
zxq1990 <disezai@vip.qq.com>
2024-09-11 09:19:39 +0800

    优化获取系统变量方法

--- Method/PublicFunction.cs
+++ Method/PublicFunction.cs
@@ -6,20 +6,24 @@ namespace PageConfig.WebApi.Method
     #region 公共方法类
     public static class PublicFunction
     {
+        //地址系统变量名称
         static readonly string AddressPathName = "SIDEAPI_ENDPOINT";
+
+        //地址
+        static string address = string.Empty;
 
         #region 获取地址系统变量
         public static string getAddress()
         {
-            string address = string.Empty;
-            try
-            {
-                address = Environment.GetEnvironmentVariable(AddressPathName, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? EnvironmentVariableTarget.Machine : EnvironmentVariableTarget.Process);
-            }
-            catch (Exception ex)
-            {
+            if (string.IsNullOrEmpty(address))
+                try
+                {
+                    address = Environment.GetEnvironmentVariable(AddressPathName, RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? EnvironmentVariableTarget.Machine : EnvironmentVariableTarget.Process);
+                }
+                catch (Exception ex)
+                {
 
-            }
+                }
             return address;
         }
         #endregion
